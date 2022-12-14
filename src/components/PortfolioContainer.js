21e56e6ba014a27d11bbacb21126407d3b89a11f
Andrew Dippel <a.t.dippel@gmail.com>
2022-12-14 21:22:31 +1100

    add initial files and start about me page

---
+++ src/components/PortfolioContainer.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import NavTabs from './NavTabs';
+import Resume from './pages/Resume';
+import About from './pages/About';
+import Projects from './pages/projects';
+import Contact from './pages/Contact';
+
+export default function PortfolioContainer() {
+  const [currentPage, setCurrentPage] = useState('About');
+
+  // This method is checking to see what the value of `currentPage` is. Depending on the value of currentPage, we return the corresponding component to render.
+  const renderPage = () => {
+    if (currentPage === 'Resume') {
+      return <Resume />;
+    }
+    if (currentPage === 'About') {
+      return <About />;
+    }
+    if (currentPage === 'Projects') {
+      return <Projects />;
+    }
+    return <Contact />;
+  };
+
+  const handlePageChange = (page) => setCurrentPage(page);
+
+  return (
+    <div>
+      {/* We are passing the currentPage from state and the function to update it */}
+      <NavTabs currentPage={currentPage} handlePageChange={handlePageChange} />
+      {/* Here we are calling the renderPage method which will return a component  */}
+      {renderPage()}
+    </div>
+  );
+}
---
+++ src/components/pages/About.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function About() {
+  return (
+    <div>
+      <h1>About Page</h1>
+      <p>
+        Junior full stack web developer with proficient skills in problem solving,
+        research, and analytical skills. I have attended a Coding bootcamp through Sydney University
+        producing projects using the following technologies HTML, CSS, JavaScript, Web APIs, Node, OOP,
+        Express, SQL, React, and many more.
+
+      </p>
+    </div>
+  );
+}
