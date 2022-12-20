42f7ec6179cdee10d9d9c587c191cdca66e38621
Andrew Dippel <a.t.dippel@gmail.com>
2022-12-20 20:17:10 +1100

    fix project buttons

--- src/components/SingleProject.js
+++ src/components/SingleProject.js
@@ -14,8 +14,8 @@ function SingleProject(props) {
                 <Card.Text>
                     {props.desc}
                 </Card.Text>
-                <Button style={{ color: '#ef6817' }} variant="link" href={props.link} target='_blank'>View Project</Button>
-                <Button style={{ color: '#ef6817' }} variant="link" href={props.githublink} target='_blank'>View github</Button>
+                <Button style={{ color: '#ef6817', marginLeft: '15%' }} variant="warning" href={props.link} target='_blank'>View Project</Button>
+                <Button style={{ color: '#ef6817', marginLeft: '15%' }} variant="warning" href={props.githublink} target='_blank'>View github</Button>
             </Card.Body>
         </Card>
     );
