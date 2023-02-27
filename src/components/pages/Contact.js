c22aaa6da90f930523c7d8a7f3c6415105f089a5
Andrew Dippel <a.t.dippel@gmail.com>
2023-02-27 20:54:54 +1100

    fix form so contact sends an email

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,7 +1,8 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
+import emailjs from '@emailjs/browser';
+
 
 export default function Contact() {
   const validate = (event) => {
@@ -40,7 +41,19 @@ export default function Contact() {
   };
   const [ismessageRequired, setIsmessageRequired] = useState(true)
 
+  const form = useRef();
 
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_f16fouj', 'template_5549dzg', form.current, 'DlPP4sfl47vn4KMsH')
+      .then((result) => {
+        console.log(result.text);
+        alert("message sent")
+      }, (error) => {
+        console.log(error.text);
+      });
+  };
 
   return (
     <Form style={{
@@ -49,10 +62,10 @@ export default function Contact() {
       backgroundColor: '#727271',
       color: 'white',
       borderRadius: '10px'
-    }}>
+    }} ref={form} onSubmit={sendEmail}>
       <Form.Group className="mb-3" controlId="formBasicName">
         <Form.Label>Name</Form.Label>
-        <Form.Control type="Name" placeholder="Full Name" onBlur={nameRequired} />
+        <Form.Control type="Name" name="name" placeholder="Full Name" onBlur={nameRequired} />
         {!isnameRequired && (
           <p style={{ color: 'red' }}>Field Is Required</p>
         )}
@@ -60,7 +73,7 @@ export default function Contact() {
 
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label>Email</Form.Label>
-        <Form.Control type="Email" size="lg" placeholder="Email" onBlur={validate} />
+        <Form.Control type="Email" name="email" size="lg" placeholder="Email" onBlur={validate} />
         {!isEmailValid && (
           <p style={{ color: 'red' }}>Invalid email</p>
         )}
@@ -68,7 +81,7 @@ export default function Contact() {
 
       <Form.Group className="mb-3" controlId="formBasicMessage">
         <Form.Label>Message</Form.Label>
-        <Form.Control type="Message" as="textarea" size="lg" placeholder="Message" onBlur={messageRequired} />
+        <Form.Control type="Message" name="message" as="textarea" size="lg" placeholder="Message" onBlur={messageRequired} />
         {!ismessageRequired && (
           <p style={{ color: 'red' }}>Field Is Required</p>
         )}
